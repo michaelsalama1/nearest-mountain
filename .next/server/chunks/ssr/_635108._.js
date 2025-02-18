8f6d6c63d2a3dc6c8a0640da6db20079518626b7
Michael S <michaelsalama19@gmail.com>
2025-02-18 18:35:50 -0500

    update

--- .next/server/chunks/ssr/_635108._.js
+++ .next/server/chunks/ssr/_635108._.js
@@ -390,7 +390,7 @@ function Home() {
                                 columnNumber: 13
                             }, this),
                             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
-                                children: "Background: Nevado Sajama, Bolivia."
+                                children: "Backgrounds: Nevado Sajama, Oruro, Bolivia & Garnet Peak, California, USA."
                             }, void 0, false, {
                                 fileName: "[project]/app/page.js",
                                 lineNumber: 187,
