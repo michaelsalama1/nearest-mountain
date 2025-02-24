96a05c89a4a38fd68bec6df8a7d2deeee654390a
Michael S <michaelsalama19@gmail.com>
2025-02-24 11:33:07 -0300

    og image

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>&#127956;</text></svg>" />
-      <meta property="og:image" content="../public/sajama.JPG" />
+      <meta property="og:image" content="/_next/static/media/sajama.c4d73355.JPG" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
